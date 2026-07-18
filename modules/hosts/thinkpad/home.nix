48682303b41785e1e910a6529c6f687c06221dc2
Fabio Castro Morffi <fabiocastromorffi@gmail.com>
2026-07-18 08:42:12 -0400

    feat: add fastfetch ascii to be generated with home + add packages for takings snips

--- modules/hosts/thinkpad/home.nix
+++ modules/hosts/thinkpad/home.nix
@@ -29,7 +29,7 @@
       ];
       initExtra = ''
           if [[ "$KITTY_WINDOW_ID" == "1" ]]; then
-            fastfetch --logo ~/Pictures/wooper_ascii.txt --structure-disabled colors --logo-position top --logo-color-1 94 --color-keys 117 --color-title 117
+            fastfetch --logo ~/.config/assets/wooper_ascii.txt --structure-disabled colors --logo-position top --logo-color-1 94 --color-keys 117 --color-title 117
           fi
 
           unset PROMPT_COMMAND
@@ -106,6 +106,11 @@
     home.packages = with pkgs; [
       slack
       yazi
+
+      # Snip
+      grim
+      slurp
+      wl-clipboard
     ];
 
     # programs.starship = {
@@ -139,6 +144,40 @@
     #     };
     #   };
     # };
+
+    home.file = {
+      # This creates a file at ~/.config/my-app/config.txt
+      ".config/assets/wooper_ascii.txt".text = ''
+                    ..........		
+               .~!7?YPPPPPPPPPJ!77.
+     :      ^7Y5JYYJ77777777JYYY55J^   ^YG. .^
+  J~ :P5^ :J5J77777777777777777777J5J: ~G@P:7#....
+.?@7 :P@7~GJ777777777777777777777777?#7 ^P@Y&GGG5!
+:G#G55P##PJ7777777777777777777777777JYP5YGBGPG#Y^
+:5#YY&#&#5!777777777777777777777777?&G##B#@? .7B!
+:Y^  ~@5GP!7777777777777777777777777?5&P^&B&5  .
+    !#P.?PY77777777777777777777777777JP? ^557
+    :!   ~PJ777777777777777777777777J#!
+          .J5J??7777777777777777??J5J:
+            :?5Y55J?77777777?YYYY5?:
+               .!~P#P??????7?5#J         ^^?555~.
+                 ?5Y7J5PP5J777?55^     ~Y55J???55!
+               .7GJ7JJ????JJ?77GYY^.77Y5?777777!BP
+               ^@?!7J5YYYY5J777!B&5YY?77777777JG]
+               .75J?YY????YY?777?BBJ?777777JJ5?:
+               .~?BYB5YY5J7JP5YJ?JY55555YY?:
+              ~PJJJYP#P55555GGPP55J
+              :?555P5!      JP?JJJP
+              ~JJJJJ!       ~JJJJJ!
+      '';
+
+      # You can also create executable scripts this way
+      ".config/assets/snip".text = ''
+          #!/usr/bin/env bash
+          grim -g "$(slurp)" - | wl-copy
+      '';
+      ".config/assets/snip".executable = true;
+    };
 
     home.stateVersion = "26.05";
   };
