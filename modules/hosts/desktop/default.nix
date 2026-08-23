16df3fa6f731b5677f5a0f57e249fd08f77fd56d
Fabio Castro Morffi <fabiocastromorffi@gmail.com>
2026-08-23 14:06:18 -0400

    fix: pkgs not available

--- modules/hosts/desktop/default.nix
+++ modules/hosts/desktop/default.nix
@@ -1,6 +1,8 @@
 { self, inputs, ...}: {
 
   flake.nixosConfigurations.desktop = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit inputs self; };
     modules = [
       self.nixosModules.desktopConfiguration
 ];
